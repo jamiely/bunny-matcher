a4aa5ca4aecf91c3f6a645cfd4a0b0291e44cbca
Jamie Ly <me@jamie.ly>
2012-11-22 12:13:28 -0500

    Added initial story board

---
+++ BunnyMatcher/Controllers/BoardViewController.m
@@ -0,0 +1,50 @@
+//
+//  BoardViewController.m
+//  BunnyMatcher
+//
+//  Created by Jamie Ly on 11/22/12.
+//  Copyright (c) 2012 Jamie Ly. All rights reserved.
+//
+
+#import "BoardViewController.h"
+
+@interface BoardViewController ()
+
+@end
+
+@implementation BoardViewController
+
+#pragma mark - Initialization
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+#pragma mark - View Controller Delegate Functions
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger) supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+@end
