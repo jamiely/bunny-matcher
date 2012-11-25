870b7920234c72ba784e2651a0c32af93e22834b
Jamie Ly <me@jamie.ly>
2012-11-24 19:04:43 -0500

    Added enemy view

---
+++ BunnyMatcher/Views/EnemyView.h
@@ -0,0 +1,13 @@
+//
+//  EnemyView.h
+//  BunnyMatcher
+//
+//  Created by Jamie Ly on 11/23/12.
+//  Copyright (c) 2012 Jamie Ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnemyView : UIView
+
+@end
---
+++ BunnyMatcher/Views/EnemyView.m
@@ -0,0 +1,31 @@
+//
+//  EnemyView.m
+//  BunnyMatcher
+//
+//  Created by Jamie Ly on 11/23/12.
+//  Copyright (c) 2012 Jamie Ly. All rights reserved.
+//
+
+#import "EnemyView.h"
+
+@implementation EnemyView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
